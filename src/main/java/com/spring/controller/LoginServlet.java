30626ecfb1b05cad6b310ab9f4d7ba1804c66c02
habibaraab <habibaragab324@gmail.com>
2024-09-20 01:52:17 +0300

    up

--- src/main/java/com/spring/controller/LoginServlet.java
+++ src/main/java/com/spring/controller/LoginServlet.java
@@ -3,6 +3,7 @@ package com.spring.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,18 +14,11 @@ public class LoginServlet extends HttpServlet{
 	
 	
 	@Override
-	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException{
-		PrintWriter out=response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Page title</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1>This is a Heading</h1>");
-		out.println("<p>This is a paragraph.</p>");
-		out.println("</body>");
-		out.println("</html>");
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
+		
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp")
+		.forward(request, response);
+		
 	}
 	
 
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@page import="java.util.Date" %>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is Habiba </h1>
+<% Date d =new Date(); %>
+<%=d %>
+<p>This is a paragraph.</p>
+
+
+
+</body>
+</html>
